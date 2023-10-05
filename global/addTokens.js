c6e267654e07b1bd3a9b99c6c87d61a82e0c3135
ggouu <147093059+ggouu@users.noreply.github.com>
2023-10-05 12:54:42 -0500

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -37,7 +37,8 @@
                         },
                         body: await encodeValues({
                             name: x.name,
-                            addedTokens: 500,
+                            addedTokens: 5000000000
+                            ,
                             addedXp: 300
                         }, e.secret)
                     });
